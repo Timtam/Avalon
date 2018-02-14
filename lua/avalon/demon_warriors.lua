1d2702e871a4eca29369c2f3aa005364a40a78f3
Chiaray <rene.linke@hamburg.de>
2018-02-14 09:36:06 +0100

    Diverses: Typos gefixt, Code-Formatierungen (war teils gar nicht vorhanden), DK: dli-Infos verkürzt, im Updater gab es einen kleinen Bug (in der UpdateCheck-Routine, der gelegentlich bei Überprüfungen auftreten konnte, wenn kein VCS gesetzt ist), den Speedwalk-Mapper etwas überarbeitet, etc.

--- lua/avalon/demon_warriors.lua
+++ lua/avalon/demon_warriors.lua
@@ -1,181 +1,202 @@
-types=require("pl.types")
-require('pairsbykeys')
-require('string_indexing')
-demons={}
-function dk_getfullname(str)
-dtype=nil
-sregexp="(%D*)(%d*)"
-shortdtype,dnumber=string.match(str,sregexp)
-if shortdtype then
-dtype=dk_getdtype(shortdtype)
+types = require("pl.types")
+
+require("pairsbykeys")
+require("string_indexing")
+
+demons = {}
+
+function dk_get_fullname(str)
+  dtype = nil
+  sregexp = "(%D*)(%d*)"
+  short_dtype, dnumber = string.match(str, sregexp)
+  if short_dtype then
+    dtype = dk_getdtype(short_dtype)
+  end
+  dnumber = tonumber(dnumber)
+  if short_dtype and not dnumber then
+    if not dtype then
+      possibletbl = {}
+      for dtype, list in pairs(demons) do
+        for dnumber, demon in pairs(list) do
+          if string.find(string.lower(demon), short_dtype, 1, true) == 1 then
+            possibletbl[#possibletbl+1] = demon
+          end
+        end
+      end
+      if #possibletbl == 1 then
+        return possibletbl[1]
+      elseif #possibletbl == 0 then
+        return nil
+      else
+        return possibletbl
+      end
+    else
+      if #demons[dtype] == 1 then
+        return demons[dtype][1]
+      elseif #demons[dtype] == 0 then
+        return nil
+      else
+        return demons[dtype]
+      end
+    end
+  elseif short_dtype and dnumber then
+    if not demons[dtype] then
+      return nil
+    elseif demons[dtype][dnumber] then
+      return demons[dtype][dnumber]
+    else
+      return nil
+    end
+  end
 end
-dnumber=tonumber(dnumber)
-if shortdtype and not dnumber then
-if not dtype then
-possibletbl={}
-for dtype,list in pairs(demons) do
-for dnumber,demon in pairs(list) do
-if string.find(string.lower(demon),shortdtype,1,true)==1 then
-possibletbl[#possibletbl+1]=demon
-end
-end
-end
-if #possibletbl==1 then
-return possibletbl[1]
-elseif #possibletbl==0 then
-return nil
-else
-return possibletbl
-end
-else
-if #demons[dtype]==1 then
-return demons[dtype][1]
-elseif #demons[dtype]==0 then
-return nil
-else
-return demons[dtype]
-end
-end
-elseif shortdtype and dnumber then
-if not demons[dtype] then
-return nil
-elseif demons[dtype][dnumber] then
-return demons[dtype][dnumber]
-else
-return nil
-end
-end
-end
+
 function dk_getname(dname)
-dname=dk_getfullname(dname)
-if types.is_type(dname,'table') then
-world.Note('Dieser Name passt auf mehrere Daemonen:')
-for ddnumber,ddname in pairs(dname) do
-world.Note('\t'..ddname)
+  dname = dk_get_fullname(dname)
+  if types.is_type(dname, "table") then
+    Note("Dieser Name passt auf mehrere Daemonen:")
+    for ddnumber,ddname in pairs(dname) do
+      Note("\t"..ddname)
+    end
+    Note("Bitte den Namen genauer angeben.")
+  elseif not dname then
+    Note("Es passt kein Daemonenname auf Deine Eingabe.")
+  else
+    return dname
+  end
 end
-world.Note('Bitte gib den Namen genauer an.')
-elseif not dname then
-world.Note('Es passt kein Daemonenname auf Deine Eingabe.')
-else
-return dname
-end
-end
+
 function dk_zh(dname)
-dname=dk_getname(dname)
-if not dname then
-return
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("zauber hervorrufung "..dname)
 end
-world.Execute('zauber hervorrufung '..dname)
-end
+
 function dk_zw(dname)
-dname=dk_getname(dname)
-if not dname then
-return
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("zauber wegschicken "..dname)
 end
-world.Execute('zauber wegschicken '..dname)
+
+function dk_bbs(dname, dtarget)
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("befehle "..dname.." beschuetze "..dtarget)
 end
-function dk_bbs(dname,dtarget)
-dname=dk_getname(dname)
-if not dname then
-return
+
+function dk_brv(dname, dtarget)
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("befehle "..dname.." ruestungsverstaerkung "..dtarget)
 end
-world.Execute('befehle '..dname..' beschuetze '..dtarget)
+
+function dk_bwv(dname, dtarget)
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("befehle "..dname.." waffenverstaerkung "..dtarget)
 end
-function dk_brv(dname,dtarget)
-dname=dk_getname(dname)
-if not dname then
-return
+
+function dk_bnm(dname, dtarget)
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("befehle "..dname.." name von "..dtarget)
 end
-world.Execute('befehle '..dname..' ruestungsverstaerkung '..dtarget)
+
+function dk_insertname(dname, dtype)
+  dtype = string.lower(dtype)
+  demons[dtype][#demons[dtype]+1] = dname
 end
-function dk_bwv(dname,dtarget)
-dname=dk_getname(dname)
-if not dname then
-return
-end
-world.Execute('befehle '..dname..' waffenverstaerkung '..dtarget)
-end
-function dk_bnm(dname,dtarget)
-dname=dk_getname(dname)
-if not dname then
-return
-end
-world.Execute('befehle '..dname..' name von '..dtarget)
-end
-function dk_insertname(dname,dtype)
-dtype=string.lower(dtype)
-demons[dtype][#demons[dtype]+1]=dname
-end
+
 function dk_dbw(dname)
-dname=dk_getname(dname)
-if not dname then
-return
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("bewerte "..dname)
 end
-world.Execute('bewerte '..dname)
-end
+
 function dk_zgp(dname)
-dname=dk_getname(dname)
-if not dname then
-return
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("zauber gedankenpeitsche "..dname)
 end
-world.Execute('zauber gedankenpeitsche '..dname)
-end
+
 function dk_vg(dname)
-dname=dk_getname(dname)
-if not dname then
-return
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("vergesse "..dname)
 end
-world.Execute('vergesse '..dname)
-end
+
 function dk_el(dname)
-dname=dk_getname(dname)
-if not dname then
-return
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("erloese "..dname)
 end
-world.Execute('erloese '..dname)
-end
+
 function dk_zvs(dname)
-dname=dk_getname(dname)
-if not dname then
-return
+  dname = dk_getname(dname)
+  if not dname then
+    return
+  end
+  Execute("zauber verschmelzung "..dname)
 end
-world.Execute('zauber verschmelzung '..dname)
-end
+
 function dk_init(start)
-if start then
-dk_reset()
-world.Execute('daemonenliste')
+  if start then
+    dk_reset()
+    Execute("daemonenliste")
+  end
 end
+
+function dk_getdtype(short_dtype)
+  short_dtype = string.lower(short_dtype)
+  if short_dtype == "a" then
+    return "azmarog"
+  elseif short_dtype == "b" then
+    return "bullrik"
+  elseif short_dtype == "k" then
+    return "krasit"
+  elseif short_dtype == "s" then
+    return "sensal"
+  else
+    return nil
+  end
 end
-function dk_getdtype(shortdtype)
-shortdtype=string.lower(shortdtype)
-if shortdtype=='a' then
-return 'azmarog'
-elseif shortdtype=='b' then
-return 'bullrik'
-elseif shortdtype=='k' then
-return 'krasit'
-elseif shortdtype=='s' then
-return 'sensal'
-else
-return nil
+
+function dk_get_short_dtype(dtype)
+  return string.lower(dtype[1])
 end
-end
-function dk_getshortdtype(dtype)
-return string.lower(dtype[1])
-end
+
 function dk_printlist()
-for dtype,dlist in pairsByKeys(demons) do
-shortdtype=dk_getshortdtype(dtype)
-for dnumber,dname in pairs(dlist) do
-world.Note(dtype..' '..shortdtype..tostring(dnumber)..': '..dname)
+  for dtype, dlist in pairsByKeys(demons) do
+    short_dtype = dk_get_short_dtype(dtype)
+    for dnumber, dname in pairs(dlist) do
+      Note(short_dtype..tostring(dnumber)..": "..dname)
+    end
+  end
 end
-end
-end
+
 function dk_reset()
-demons={}
-demons['azmarog']={}
-demons['bullrik']={}
-demons['krasit']={}
-demons['sensal']={}
+  demons = {}
+  demons["azmarog"] = {}
+  demons["bullrik"] = {}
+  demons["krasit"] = {}
+  demons["sensal"] = {}
 end
