d4e79220db0751fbcf92cc72ef9e23b280b52325
Toni Barth <toni.barth@gisa.de>
2018-04-18 09:39:45 +0200

    pfadfilterung auf sich gegenseitig negierende wege hinzugefügt (noch nicht getestet)

--- lua/avalon/speedwalking/constants.lua
+++ lua/avalon/speedwalking/constants.lua
@@ -1,4 +1,16 @@
 c = {
+  DIRECTIONS = {
+    o = "w",
+    so = "nw",
+    s = "n",
+    sw = "no",
+    w = "o",
+    nw = "so",
+    n = "s",
+    no = "sw",
+    hoch = "runter",
+    runter = "hoch"
+  },
   SPEEDWALK_REGEX = "([a-zA-Z0-9%.]+)_([a-zA-Z0-9%.]+)",
   STATION_REGEX = "([a-zA-Z0-9]*)%.?([a-zA-Z0-9]*)",
   WALK_SPEED = 0.3,
--- lua/avalon/speedwalking/utils.lua
+++ lua/avalon/speedwalking/utils.lua
@@ -1,7 +1,34 @@
+Const = require("avalon.speedwalking.constants")
 List = require("pl.list")
-utils = {}
+String = require("pl.stringx")
+Utils = {}
 
-utils.path_length = function(l)
+Utils.is_inverse = function(dir1, dir2)
+  dir1 = dir1:lower()
+  dir2 = dir2:lower()
+  if dir1:len() ~= dir2:len() then
+    return false
+  end
+  -- we separate everything identical from those two directions
+  -- e.g. kletter_, schwimm_
+  local i
+  local ident = ""
+  for i = 1, String.len(dir1) do
+    if String.at(dir1, i) == String.at(dir2, i) then
+      ident = ident + String.at(dir1, i)
+    else
+      break
+    end
+  end
+  dir1 = dir1:sub(ident:len()+1)
+  dir2 = dir2:sub(ident:len()+1)
+  if Const.DIRECTIONS[dir1] == dir2 then
+    return true
+  end
+  return false
+end
+
+Utils.path_length = function(l)
   len = 0
   for i = 1, l:len() - 1 do
     len = len + l[i]:find_path(l[i+1]):get_length()
@@ -9,12 +36,21 @@ utils.path_length = function(l)
   return len
 end
 
-utils.resolve_path = function(l)
+Utils.resolve_path = function(l)
   p = List.new()
+  local i
   for i = 1, l:len() - 1 do
     p:extend(l[i]:find_path(l[i+1]).path)
   end
+  i = 1
+  repeat
+    if Utils.is_inverse(p[i], p[i+1]) then
+      p:chop(i,i+1)
+    else
+      i = i + 1
+    end
+  until i > p:len()
   return p
 end
 
-return utils
+return Utils
