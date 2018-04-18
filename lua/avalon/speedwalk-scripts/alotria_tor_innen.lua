268227a2213fe117e2cf57ea1714844a37526dbd
Toni Barth <software@satoprogs.de>
2018-04-18 20:10:15 +0200

    alotria tor von innen öffnen script umgeschrieben, um schlechte programmierung der adeligen abzufangen (das tor ist zu, aber man kann den hebel (noch) nicht ziehen)

--- lua/avalon/speedwalk-scripts/alotria_tor_innen.lua
+++ lua/avalon/speedwalk-scripts/alotria_tor_innen.lua
@@ -6,7 +6,7 @@ Class.Avalon_Alotria_Tor_Innen(Script)
 
 function Avalon_Alotria_Tor_Innen:setup()
   world.AddTriggerEx("avalon_alotria_tor_innen_offen",
-                     "Da gerade erst am Hebel gezogen wurde, wartest Du erst einmal ab.",
+                     "*Es ist weit geoeffnet.*",
                      "speedwalk_active_script():success()",
                      trigger_flag.Enabled,
                      NOCHANGE,
@@ -16,7 +16,7 @@ function Avalon_Alotria_Tor_Innen:setup()
                      sendto.script,
                      100
                     )
-  world.AddTriggerEx("avalon_alotria_tor_innen_geschlossen",
+  world.AddTriggerEx("avalon_alotria_tor_innen_geoeffnet",
                      "Laut rasselnd oeffnet sich das Tor.",
                      "speedwalk_active_script():success()",
                      trigger_flag.Enabled,
@@ -27,16 +27,45 @@ function Avalon_Alotria_Tor_Innen:setup()
                      sendto.script,
                      100
                     )
-  self:add_command("ziehe hebel")
+  world.AddTriggerEx("avalon_alotria_tor_innen_geschlossen",
+                     "*Es ist geschlossen.*",
+                     "speedwalk_active_script():ziehe_hebel()",
+                     trigger_flag.Enabled,
+                     NOCHANGE,
+                     0,
+                     '',
+                     '',
+                     sendto.script,
+                     100
+                    )
+  self.time = get_unix_time()
+  self:add_command("betrachte tor")
 end
 
 function Avalon_Alotria_Tor_Innen:teardown()
-  world.DeleteTrigger("avalon_alotria_tor_innen_offen")
+  world.DeleteTrigger("avalon_alotria_tor_innen_geoeffnet")
   world.DeleteTrigger("avalon_alotria_tor_innen_geschlossen")
+  world.DeleteTrigger("avalon_alotria_tor_innen_offen")
 end
 
 function Avalon_Alotria_Tor_Innen:inverts(s)
   return Types.type(s) == 'Avalon_Alotria_Tor_Aussen'
+end
+
+function Avalon_Alotria_Tor_Innen:ziehe_hebel()
+  self:add_command("ziehe hebel")
+  self.time = get_unix_time()
+end
+
+function Avalon_Alotria_Tor_Innen:pop_command()
+  cmd = self._base.pop_command(self)
+  if not Types.is_empty(cmd) then
+    return cmd
+  end
+  if get_unix_time()-self.time > 5 then
+    self:ziehe_hebel()
+    return self._base.pop_command(self)
+  end
 end
 
 return Avalon_Alotria_Tor_Innen
