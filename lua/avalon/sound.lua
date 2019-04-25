0012aabdd300271abe54c9d15372ea6134641763
Toni Barth <toni.barth@gisa.de>
2019-04-25 10:43:07 +0200

    sounds werden nach dem Abspielen automatisch freigegeben

--- lua/avalon/sound.lua
+++ lua/avalon/sound.lua
@@ -11,11 +11,13 @@ local sound_init = function()
   BASS:Init()
 end
 
-local sound_play_sound = function(file, pan, vol)
+local sound_play_sound = function(file, pan, vol, no_free)
 
   pan = pan or 0
 
   vol = vol or Avalon.GetConfig("settings", "SoundVolume")
+
+  no_free = Types.to_bool(no_free)
 
   if Avalon.GetConfig("settings", "SoundsMuted") == 1 then
     return
@@ -29,7 +31,13 @@ local sound_play_sound = function(file, pan, vol)
     file = GetInfo(74) .. file
   end
 
-  local snd = BASS:StreamCreateFile(false, file)
+  local flags = 0
+
+  if no_free == false then
+    flags = Audio.CONST.stream.auto_free
+  end
+
+  local snd = BASS:StreamCreateFile(false, file, 0, 0, flags)
   snd:SetAttribute(Audio.CONST.attribute.volume, vol / 100)
   snd:SetAttribute(Audio.CONST.attribute.pan, pan)
   snd:Play()
