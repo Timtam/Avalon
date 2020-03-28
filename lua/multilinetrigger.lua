67d416563cd288833cc4229f9fd24f2a88f1590e
Toni Barth <software@satoprogs.de>
2020-03-28 15:42:09 +0100

    fehler bei falsch gruppierten Multiline-Triggern behoben fixes #25

--- lua/multilinetrigger.lua
+++ lua/multilinetrigger.lua
@@ -6,8 +6,13 @@ function MultilineTrigger:_init(sound,group,sequence)
   self.lines={}
   self.text=''
   self.sound=sound or ''
+  -- the trigger group, but only the first trigger will be part of this group
   self.group=group or ''
   self.sequence=sequence or 100
+  -- all other triggers will be part of this group. that ensures that the 
+  -- first trigger can enable this group and the last one can disable it 
+  -- without disabling the first trigger as well
+  self.trigger_group = world.GetUniqueID()
 end
 
 function MultilineTrigger:Line(text,regexp,omit)
@@ -24,15 +29,8 @@ function MultilineTrigger:Line(text,regexp,omit)
 end
 
 function MultilineTrigger:Inject()
-  local groupname = utils.hash(tostring(string.len(self.text))..'\r\n'..self.text)
-  local group
+  local name = utils.hash(tostring(string.len(self.text))..'\r\n'..self.text)
   local i
-
-  if self.group == '' then
-    group = groupname
-  else
-    group = self.group
-  end
 
   for i = 1, #self.lines, 1 do
     local flags = 0
@@ -45,18 +43,19 @@ function MultilineTrigger:Inject()
       flags=flags+trigger_flag.OmitFromOutput
     end
 
-    local tgroupname='t_'..groupname..'_'..tostring(i)
+    local tname='t_'..name..'_'..tostring(i)
     if i==1 then
       if self.group=='' then
         flags=flags+trigger_flag.Enabled
       end
-      world.AddTriggerEx(tgroupname,self.lines[i].text,'world.EnableGroup("'..group..'",1)',flags,NOCHANGE,0,'','',sendto.script,self.sequence)
+      world.AddTriggerEx(tname,self.lines[i].text,'world.EnableGroup("'..self.trigger_group..'",1)',flags,NOCHANGE,0,'','',sendto.script,self.sequence)
+      world.SetTriggerOption(tname,'group',self.group)
     elseif i==#self.lines then
-      world.AddTriggerEx(tgroupname,self.lines[i].text,'world.EnableGroup("'..group..'",0)',flags,NOCHANGE,0,self.sound,'',sendto.script,self.sequence)
-      world.SetTriggerOption(tgroupname,'group',group)
+      world.AddTriggerEx(tname,self.lines[i].text,'world.EnableGroup("'..self.trigger_group..'",0)',flags,NOCHANGE,0,self.sound,'',sendto.script,self.sequence)
+      world.SetTriggerOption(tname,'group',self.trigger_group)
     else
-      world.AddTriggerEx(tgroupname,self.lines[i].text,'',flags,NOCHANGE,0,'','',sendto.script,self.sequence)
-      world.SetTriggerOption(tgroupname,'group',group)
+      world.AddTriggerEx(tname,self.lines[i].text,'',flags,NOCHANGE,0,'','',sendto.script,self.sequence)
+      world.SetTriggerOption(tname,'group',self.trigger_group)
     end
   end
 end
